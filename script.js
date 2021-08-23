9a920fedc8dbacb964be55514d95ab07db463b70
lyndon dsouza <lyndondsouza1@gmail.com>
2021-08-23 20:52:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+const url = "https://randomuser.me/api/";
+
+// const randomUser = document.createElement("div");
+// document.body.appendChild(randomUser);
+const mainEl = document.getElementById("main");
+const container = document.getElementById("container");
+
+async function getData(url) {
+	const response = await fetch(url);
+	var data = await response.json();
+	console.log(data.results[0]);
+	const user = data.results[0];
+	random(user);
+}
+getData(url);
+
+function random(results) {
+	const randomuser = document.createElement("div");
+	randomuser.classList.add("randomuser");
+
+	randomuser.innerHTML = `
+    
+    <div class="img-container">
+    <img src="${results.picture.large}">
+</div>
+   <div id="phone" class="tabcontent">
+My Phone number is
+	<br />
+   <p>${results.phone}<p>
+   </div>
+   <div id="name" class="tabcontent">
+My Name is 	<br />
+ <p> ${results.name.first} ${results.name.last}</p>
+    </div>
+   <div id="dob" class="tabcontent">
+My DOB  is 	<br />
+    <p> ${results.dob.date}</p>
+    </div>
+   <div id="address" class="tabcontent">
+My Address is	<br />
+    <p> ${results.location.country}</p>
+    </div>
+   <div id="email" class="tabcontent">
+My Email Address is 	<br />
+  <p> ${results.email}</p>
+    </div>
+  
+
+    `;
+
+	// const btn = randomuser.querySelector(".user-body");
+
+	// btn.addEventListener("click", () => {
+	// 	getData(url);
+	// });
+	container.appendChild(randomuser);
+	document.getElementById("defaultOpen").onmouseover();
+}
+function openTab(evt, tabname) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	document.getElementById(tabname).style.display = "block";
+	evt.currentTarget.className += " active";
+}
